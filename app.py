2df9403fa9df1f4d6afc203dfe63a42dcd816981
Niharika-Ann8 <niharikaannkishore@gmail.com>
2026-02-21 17:39:28 +0530

    html index

--- app.py
+++ app.py
@@ -12,6 +12,10 @@ supabase = create_client(url, key)
 
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return render_template("index.html")
+
 @app.route("/dashboard")
 def dashboard():
     return render_template("dashboard.html")
